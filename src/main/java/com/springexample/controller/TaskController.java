f4c50084d659e1bd6e3cfd3b874a216ccb5f422a
Kyel John David <kyeljohnmdavid@gmail.com>
2013-07-27 20:56:43 +0800

    Added Session Attributes for TaskController

--- src/main/java/com/springexample/controller/TaskController.java
+++ src/main/java/com/springexample/controller/TaskController.java
@@ -1,12 +1,19 @@
 package com.springexample.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.springexample.forms.TaskForm;
 
 
+@Controller
+@SessionAttributes(types = {TaskForm.class})
 public interface TaskController {
 
 	@RequestMapping(value = "create" ,method = RequestMethod.GET)
-	String createForm();
-
+	ModelAndView createForm();
+	
 }
