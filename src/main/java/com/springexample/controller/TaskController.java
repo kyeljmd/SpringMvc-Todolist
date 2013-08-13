e6a517e586f96021d09eb81d53264ad535923aab
Kyel John David <kyeljohnmdavid@gmail.com>
2013-08-13 20:14:23 +0800

    Updated the TaskController

--- src/main/java/com/springexample/controller/TaskController.java
+++ src/main/java/com/springexample/controller/TaskController.java
@@ -2,6 +2,7 @@ package com.springexample.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
@@ -18,4 +19,7 @@ public interface TaskController {
 	
 	@RequestMapping(value = "save", method = RequestMethod.POST)
 	ModelAndView saveTask(TaskForm task,BindingResult result);
+	
+	@RequestMapping(value = "view/{taskId}", method = RequestMethod.GET)
+	ModelAndView viewTask(@PathVariable("taskId") Long taskId);
 }
